e6966f03951c2d09449f13da57aac9639cad2674
shubham-rakshe <shubhamrakshe92@gmail.com>
2022-08-11 20:30:05 +0530

    fact file updated

--- JavaPrograms/src/JP/Fact.java
+++ JavaPrograms/src/JP/Fact.java
@@ -4,7 +4,7 @@ public class Fact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          int num=6;
+          int num=7;
           int fact = 1;
         
           for(int i = 1;i<=num;i++)
